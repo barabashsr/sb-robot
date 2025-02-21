224a838c2c2407161e480c49c33fd0e5377661f0
Sergey Barabash <barabashsr@gmail.com>
2025-02-22 00:59:35 +0300

    camera test optimized

--- esp32/camera_test/src/main.cpp
+++ esp32/camera_test/src/main.cpp
@@ -116,6 +116,11 @@ void startCameraServer(){
   }
 }
 
+void streamTask(void *pvParameters) {
+  startCameraServer();
+  vTaskDelete(NULL);
+}
+
 void setup() {
   WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0);
   
@@ -145,9 +150,40 @@ void setup() {
   config.pin_reset = RESET_GPIO_NUM;
   config.xclk_freq_hz = 20000000; // Set XCLK frequency to 20 MHz
   config.pixel_format = PIXFORMAT_JPEG; // Use JPEG format for streaming
-  config.frame_size = FRAMESIZE_UXGA; // Set frame size (modify as needed)
-  config.jpeg_quality = 12; // Set JPEG quality (lower is better quality)
-  config.fb_count = 1; // Number of frame buffers to use
+  config.frame_size = FRAMESIZE_VGA; // Set frame size (modify as needed)
+  config.jpeg_quality = 20; // Set JPEG quality (lower is better quality)
+  config.fb_count = 2; // Number of frame buffers to use
+  //config.grab_mode = CAMERA_GRAB_LATEST; // Always grab the latest frame
+  config.fb_location = CAMERA_FB_IN_DRAM;
+/* 
+  camera_config_t config;
+config.ledc_channel = LEDC_CHANNEL_0;
+config.ledc_timer = LEDC_TIMER_0;
+config.pin_d0 = Y2_GPIO_NUM;
+config.pin_d1 = Y3_GPIO_NUM;
+config.pin_d2 = Y4_GPIO_NUM;
+config.pin_d3 = Y5_GPIO_NUM;
+config.pin_d4 = Y6_GPIO_NUM;
+config.pin_d5 = Y7_GPIO_NUM;
+config.pin_d6 = Y8_GPIO_NUM;
+config.pin_d7 = Y9_GPIO_NUM;
+config.pin_xclk = XCLK_GPIO_NUM;
+config.pin_pclk = PCLK_GPIO_NUM;
+config.pin_vsync = VSYNC_GPIO_NUM;
+config.pin_href = HREF_GPIO_NUM;
+config.pin_sccb_sda = SIOD_GPIO_NUM;
+config.pin_sccb_scl = SIOC_GPIO_NUM;
+config.pin_pwdn = PWDN_GPIO_NUM; // Use PWDN pin for power-down mode
+config.pin_reset = RESET_GPIO_NUM;
+
+config.xclk_freq_hz = 10000000;       // Lower XCLK frequency to reduce heat
+config.pixel_format = PIXFORMAT_JPEG; // Use JPEG format for lower processing load
+config.frame_size = FRAMESIZE_SVGA;   // Reduce resolution to SVGA
+config.jpeg_quality = 15;             // Increase JPEG compression quality
+config.fb_count = 1;                  // Use one frame buffer to reduce memory usage
+
+
+ */
   
   // Camera init
   esp_err_t err = esp_camera_init(&config);
@@ -169,7 +205,8 @@ void setup() {
   Serial.println(WiFi.localIP());
   
   // Start streaming web server
-  startCameraServer();
+  //startCameraServer();
+  xTaskCreatePinnedToCore(streamTask, "StreamTask", 8192, NULL, 1, NULL, 1); // Pin to Core 1
 }
 
 void loop() {
